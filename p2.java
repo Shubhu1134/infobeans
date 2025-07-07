ee15d3ab6c79d9a8c4e02686912581c827b04e65
Shubhu1134 <shubhu1134@gmail.com>
2025-07-07 22:40:52 +0530

    Add p1, p2, p3 Java files and remove old Test.java

---
+++ p2.java
@@ -0,0 +1,10 @@
+class p2 {
+public static void main (String args[]){
+    System.out.println("A\n");
+    System.out.println("B\n");
+    System.out.println("C\n");
+    System.out.println("\\n \\t \\a \\r \\b \\z");
+    System.out.println("\" welcome\"");
+} 
+    
+}
