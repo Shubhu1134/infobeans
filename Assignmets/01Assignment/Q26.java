612d2ccaff4b835c0306d622758cba2e70b17c5e
Shubhu1134 <shubhu1134@gmail.com>
2025-07-10 15:20:40 +0530

    Update solutions from Q26 to Q45

--- Assignmets/01Assignment/Q26.java
+++ Assignmets/01Assignment/Q26.java
@@ -3,10 +3,19 @@ lay a path 120 m long and 2.4 m breadth
 if a brick is 24 cm long and 15 cm wide?
 */ 
 class Test {
-    public static void main(String args[]{
-      int  pathlength = 12000 ;
-      int  pathbreadth = 240;
-      int  bricklength = 24;
-      int  brickbridth = 15;
-    })
-}
+    public static void main(String args[]){
+      double  pathlength = 12000 ;
+      double  pathbreadth = 240;
+      double  bricklength = 24;
+      double  brickbridth = 15;
+
+
+      double patharea = pathlength * pathbreadth;
+      double brickarea = bricklength * brickbridth;
+
+      double noOfbrick = patharea/brickarea;
+
+      System.out.println ("no of brick lay at total area of path : " + noOfbrick);
+    }
+
+    }
