13532efc19a3e8d9e76a8ad85dd21d00685086d3
shubham <99715758+Shubhu1134@users.noreply.github.com>
2025-11-29 20:29:01 +0530

    Update Q24.java
    try to solve with new approach 
    old question

--- Assignmets/Q24.java
+++ Assignmets/Q24.java
@@ -2,3 +2,47 @@
 10 cm wide and 7.5 cm thick will be required for a wall 20 m long, 2 m high and 0.75 m thick? 
 If bricks sell at $900 per thousand what will it cost to build the wall?
 */ 
+
+class Brick{
+private int blong;
+private int bwide;
+private int bthick;
+
+Brick(){}
+
+Brick(int blong, int bwide,int bthick){
+blong=this.blong;
+bwide=this.bwide;
+bthick=this.bthick;
+}
+
+public int BrickVol(){
+
+}
+
+}
+
+class Wall{
+
+private int wlong;
+private int whigh;
+private int wthick;
+
+Wall(){}
+
+Wall(int wlong,int wthick,int whigh){
+wlong=this.blong;
+wthick=this.thick;
+whigh=this.whigh;
+
+}
+
+
+}
+}
+
+class Test {
+public static void main(String args[]){
+
+}
+}
